be2e6b856b6dd1124448d7d34acbdee58bfa5e49
PabloAbreu1 <pagustin2404@gmail.com>
2023-01-16 15:27:50 +0000

    Spotify

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect, useRef} from "react";
 
 //create your first component
 const Home = () => {
+	const audioElement = useRef(null);
+	const [song, setSongs] =useState([]);
+	
+  function getSongs() {
+
+    fetch("https://assets.breatheco.de/apis/sound/songs")
+    .then(response => response.json())
+    .then(data => {
+    (setSongs(data)) 
+      console.log(data);
+    })
+  
+  }
+
+  useEffect(() =>{
+    getSongs() 
+  
+  }, [])
+
+  const playSong = (url) => {
+    audioElement.current.src = "https://assets.breatheco.de/apis/sound/" + url
+    console.log(audioElement.current.src);
+    console.log(url);
+  
+    
+  };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<h1>Spotify</h1>
+		<h2>Canciones:</h2>
+{/* dibujamos la lista de canciones */}
+		<ul>
+			{song.map((oneSong, index)=><li onClick={ ()=>  playSong(oneSong.url)} key={oneSong.name}>{oneSong.name}</li>)}
+		</ul>
+		<div>
+
+		<audio controls src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3" ref={audioElement}>
+  <source type="audio/ogg"/>
+  {/* <source src="horse.mp3" type="audio/mpeg"> */}
+  Your browser does not support the audio element.
+</audio>
+
+
 		</div>
-	);
+		</>
+    )
 };
+
 
 export default Home;
